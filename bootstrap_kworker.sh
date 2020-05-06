369ad85da8600d8de2d1c2983184bb1f541cb30a
bsullin <bsullin@rei.com>
2020-05-06 09:27:51 -0700

    Altered the kworker script to use the new domain.

--- bootstrap_kworker.sh
+++ bootstrap_kworker.sh
@@ -3,5 +3,5 @@
 # Join worker nodes to the Kubernetes cluster
 echo "[TASK 1] Join node to Kubernetes Cluster"
 sudo yum install -q -y sshpass
-sshpass -p "vagrant" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.example.com:/joincluster.sh /tmp/joincluster.sh
+sshpass -p "vagrant" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.behemoth.local:/joincluster.sh /tmp/joincluster.sh
 sudo bash /tmp/joincluster.sh
